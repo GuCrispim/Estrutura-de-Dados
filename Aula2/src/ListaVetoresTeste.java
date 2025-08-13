8569135b9b5815b6c653d05168ba9a0a03a136de
GugaCrispim <gustavocrispimtec@gmail.com>
2025-08-13 19:20:21 -0300

    Zeca pegou os codigos

--- Aula2/src/ListaVetoresTeste.java
+++ Aula2/src/ListaVetoresTeste.java
@@ -1,3 +1,10 @@
 public class ListaVetoresTeste {
-
+    public static void main(String[] args) {
+        ListaVetores lista = new ListaVetores(5);
+        lista.insere(66);
+        lista.insere(77);
+        lista.insere(88);
+        lista.insere(99);
+        lista.insere(100);
+    }
 }
