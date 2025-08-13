af968c82f44854f9fb0623e1af1183a4e6deb4aa
GugaCrispim <gustavocrispimtec@gmail.com>
2025-08-13 19:42:16 -0300

    teste

--- Aula2/src/ListaVetores.java
+++ Aula2/src/ListaVetores.java
@@ -1,32 +1,41 @@
 public class ListaVetores {
-        private Integer[] dados;
-        private int qt;
+    private Integer[] dados;
+    private int qt;
 
-        ListaVetores(int tam){
-            dados = new Integer[tam];
-            qt = 0;
+    ListaVetores(int tam) {
+        dados = new Integer[tam];
+        qt = 0;
+    }
+
+    void insere(int valor) {
+        if (qt < dados.length) {
+            dados[qt] = valor;
+            qt++;
         }
+    }
 
-        void insere(int valor){
-            if(qt < dados.length){
-                dados[qt] = valor;
-                qt++;
-            }
+    void imprime() {
+
+        for (int i = 0; i < qt; i++) {
+            System.out.print(dados[i] + " -> ");
         }
+    }
 
-        void imprime(){
-
-            for(int i = 0; i < qt; i++) {
-                System.out.print(dados[i] + " -> ");
-            }
-        }
-
-        Integer removeFinal(){
-            if(qt == 0) return null;
-            Integer temp = dados[qt - 1];
-            qt --;
-            return temp;
-
-        }
+    Integer removeFinal() {
+        if (qt == 0) return null;
+        Integer temp = dados[qt - 1];
+        qt--;
+        return temp;
 
     }
+
+    Integer removePosicao(int pos) {
+        if (pos < 0 || pos >= qt) return null;
+
+        for(int i = pos + 1; i<qt; i++){
+            dados[i-1] = dados[i];
+            qt--;
+        }
+        return temp;
+    }
+}
